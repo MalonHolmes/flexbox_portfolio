cb685ba11632f299a060b7c567a0a3d53e43de78
Student 4 <student4@Codes-MacBook-Air-5.local>
2023-05-28 00:50:51 -0400

    Created a page with a flex container and multiple flex items inside it

---
+++ script.js
@@ -0,0 +1,7 @@
+/* window.addEventListener('scroll', function() {
+    var scrolled = window.pageYOffset;
+    var background = document.getElementById('background');
+    var hue = scrolled / (document.body.scrollHeight - window.innerHeight) * 360; // Adjust the color range if needed
+    background.style.backgroundColor = 'hsl(' + hue + ', 100%, 50%)';
+  }); */
+
