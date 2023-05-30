ff942b5f862edc1bcf050158c8cf303689f65f4f
Student 4 <student4@Codes-MacBook-Air-5.local>
2023-05-30 01:18:24 -0400

    Portfolio - Mach 1 complete - created general layout, no functionality

--- script.js
+++ script.js
@@ -5,3 +5,25 @@
     background.style.backgroundColor = 'hsl(' + hue + ', 100%, 50%)';
   }); */
 
+
+// Open the modal when the image is clicked
+var image = document.querySelector(".image-popup");
+image.addEventListener("click", function() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+});
+
+// Close the modal when the close button is clicked
+var closeBtn = document.querySelector(".close");
+closeBtn.addEventListener("click", function() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+});
+
+// Close the modal when clicking outside the modal content
+window.addEventListener("click", function(event) {
+  var modal = document.getElementById("myModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
